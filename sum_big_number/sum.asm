2e9f39668ead4c8950b5f421eafa6f8b3882ab45
Zambo-dev <thomasz01@outlook.it>
2022-03-04 16:29:33 +0100

    Added comments to sum.asm

--- sum_big_number/sum.asm
+++ sum_big_number/sum.asm
@@ -1,3 +1,8 @@
+; This function is based on big-endian numbers, so it start computing using the first byte
+; from the right
+; It will be optimized and then used in the project Zambo-dev/big-number-toolbox-library to
+; archive better performance
+
 	section .text
 	global sum
 
@@ -7,12 +12,12 @@ sum:
 	; Move esp -> ebp, so ebp point to the pushed ebp
 	mov 	ebp, esp
 	
-	; Push some preserved regiesters
+	; Push some preserved regiesters into the stack
 	push 	ebx
 	push 	esi
 	push	edi
 	
-	; Edit pointer value
+	; Store function parameters into registers
 	mov	ecx, [ebp+16]		; Get number size from stack
 	mov 	eax, [ebp+12]		; Get val2 address from stack
 	mov	ebx, [ebp+8]		; Get val1 address from stack
@@ -23,12 +28,12 @@ sum:
 	add	ebx, ecx
 	dec	ebx
 	
-	; Make space in the stack for pushing ecx and for the carrys	
+	; Make space in the stack for ecx and for the two carry variables
 	sub 	esi, 12
 
-	; Set carrys to 0
-	mov	dword [ebp-20], 0
-	mov	dword [ebp-24], 0
+	; Set carry variables to 0
+	mov	dword [ebp-20], 0	; ebp-20 is the tmp_carry, the carry for the next byte
+	mov	dword [ebp-24], 0	; ebp-24 si the carry for the actual byte
 
 	loop:
 		; Push ecx into the stack
@@ -38,40 +43,45 @@ sum:
 		xor	ecx, ecx
 		xor 	edx, edx
 		
-		; Add bytes
+		; Store the least byte from memory into the least byte into registers
 		mov	dl, byte [ebx]
 		mov	cl, byte [eax]
 		add	edx, ecx	
 	
-		; Store result
+		; Store the least byte as a result
 		mov	byte [ebx], dl	
-		; Set tmp_carry
-		mov	byte [ebp-20], dh	; Move the new carry in tmp_carry				
+		; Store the second-least byte as tmp_carry
+		mov	byte [ebp-20], dh				
 			
 		; Clear ecx and edx
 		xor	ecx, ecx
 		xor	edx, edx
 
-		; Add byte with carry
+		; Store the least byte from memory into the least byte into registers
 		mov	dl, byte [ebx]
 		mov	cl, byte [ebp-24]
 		add	edx, ecx
 	
-		; Store result	
+		; Store the least byte as a result
 		mov	byte [ebx], dl
 		
-		; Set carry
+		; Store the second least byte into cl
 		mov	cl, byte [ebp-20]
+		; Add the carry in dh into cl
 		add	cl, dh
+		; Store the carry for the next byte
 		mov	byte [ebp-24], cl 
 			
 		; Pop ecx from the stack
 		pop	ecx
 		
+		; Decrement the size
 		dec	ecx		; Sub 1 from ecx
+		; Point to the next byte
 		dec	eax		; Dec address of val 2
 		dec	ebx		; Dec address of val 1
 		
+		; Check if the sum is at the end
 		cmp	ecx, 0
 		jne	loop
 	
